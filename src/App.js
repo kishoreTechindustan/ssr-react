05edfb2a6ca6692f8d3bc335bb9f899a5146ce14
kishoreTechindustan <kishore@techindustan.com>
2020-07-22 15:51:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './Home'
+function App() {
+  return (
+    <div className="App">
+        <Home name="Sammy" />
+    </div>
+  );
+}
+
+export default App;
